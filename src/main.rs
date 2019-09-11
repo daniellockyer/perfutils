fc8fa227d431fe3a84dc232434f6879c6bf7a018
Daniel Lockyer <hi@daniellockyer.com>
2019-09-11 18:03:23 +0100

    Shorten the code

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,26 @@
-#![feature(proc_macro_hygiene, decl_macro, type_ascription)]
+#![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use]
 extern crate rocket;
 
-use std::io::{self, Cursor, Seek, SeekFrom};
+use std::io::Seek;
 use inferno::collapse::Collapse;
-use rocket::Data;
 
 #[post("/upload", data = "<data>")]
-fn upload(data: Data) -> io::Result<String> {
+fn upload(data: rocket::Data) -> std::io::Result<String> {
     let xd_opts = inferno::collapse::xdebug::Options::default();
     let mut fg_opts = inferno::flamegraph::Options::default();
 
-    let mut buff = Cursor::new(Vec::new());
-    let mut buff2 = Cursor::new(Vec::new());
-    let mut buff3 = Cursor::new(Vec::new());
+    let mut buff = std::io::Cursor::new(Vec::new());
+    let mut buff2 = std::io::Cursor::new(Vec::new());
+    let mut buff3 = std::io::Cursor::new(Vec::new());
 
     data.stream_to(&mut buff)?;
 
-    buff.seek(SeekFrom::Start(0))?;
+    buff.seek(std::io::SeekFrom::Start(0))?;
     inferno::collapse::xdebug::Folder::from(xd_opts).collapse(buff, &mut buff2)?;
 
-    buff2.seek(SeekFrom::Start(0))?;
+    buff2.seek(std::io::SeekFrom::Start(0))?;
     let _ = inferno::flamegraph::from_reader(&mut fg_opts, buff2, &mut buff3);
 
     let inner = buff3.into_inner();
